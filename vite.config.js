a1fddcfa29dd656ae9a1c2ba26d272748f2b0ea1
Soufiane <jabersoufiane605@gmail.com>
2024-08-29 17:37:43 +0100

    remove PWA

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
-// https://vitejs.dev/config/
+
 export default defineConfig({
     base: "/Workout-Plan/",
-    plugins: [
-        react(),
-        VitePWA({
-            registerType: "autoUpdate",
-            includeAssets: [],
-            manifest: {
-                name: "Workout Plan",
-                short_name: "W-Plan",
-                description: "An App to help you organize your workouts",
-                theme_color: "#ffffff",
-                icons: [
-                    {
-                        src: "gym.png",
-                        sizes: "192x192",
-                        type: "image/png",
-                    },
-                ],
-            },
-        }),
-    ],
+    plugins: [react()],
 });
