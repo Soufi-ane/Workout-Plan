5962391386ac79f45fc2d376c25d8df92d48742a
Soufiane <jabersoufiane605@gmail.com>
2024-09-03 12:41:26 +0100

    fix styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,8 @@
-import { TbMenuDeep } from "react-icons/tb";
 import { useNavigate } from "react-router-dom";
 const Header = () => {
     const navigate = useNavigate();
     return (
-        <div className="flex absolute bg-white justify-between items-center z-50 w-screen px-3 py-2">
+        <div className="flex absolute bg-white justify-between items-center z-40 w-screen px-3 py-2">
             <img
                 onClick={() => {
                     navigate("/Workout-Plan");
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,6 @@
 export default function Modal({ children, onConfirm, onClose }) {
     return (
-        <div className="absolute z-20 top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 flex bg-stone-400 backdrop-blur-sm bg-opacity-50 items-center justify-center w-screen h-[100dvh]">
+        <div className="absolute z-50 top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 flex bg-stone-400 backdrop-blur-sm bg-opacity-50 items-center justify-center w-screen h-[100dvh]">
             <div className="bg-white rounded-md py-3 px-5 w-11/12">
                 <div>{children}</div>
                 <div className="flex items-center justify-between px-3 gap-5">
